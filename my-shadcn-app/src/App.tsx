ac5df0d1edb4b747cbeae76d0026e848f18cb967
Manuella Perlin <dbperlin@debian-ideapad.DBPerlin>
2026-02-10 14:17:16 -0300

    Primeiras mudanças

--- my-shadcn-app/src/App.tsx
+++ my-shadcn-app/src/App.tsx
@@ -1,90 +1,114 @@
 import './App.css'
-import * as React from "react"
+//import * as React from "react"
 import { Link } from "react-router-dom"
-//import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button"
 import {
   NavigationMenu,
-  NavigationMenuContent,
+  //NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
+  //NavigationMenuList,
+  //NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu"
 
 function App() {
   return (
     <>
-    <NavigationMenuDemo />
-      <TypographyH1 />
-      <TypographyH2 />
+      <NavMenu />
+      <main className="pt-16">
+        <nav className="fixed top-55 left-0 right-0 bg-white">
+        <TypographyH4 />
+        </nav>
+        <nav className="fixed top-70 left-0 right-0 bg-white">
+        <TypographyH1 />
+        </nav>
+        <nav className="fixed top-100 left-0 right-0 bg-white">
+        <TypographyH2 />
+        </nav>
+      </main>
     </>
   )
 }
 
 export function TypographyH1() {
   return (
+
     <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
-      Manuella Perlin
+      Bem-vindo ao meu site
     </h1>
   )
 }
 
 export function TypographyH2() {
   return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-      Sobre Mim
+    <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+      Deixe os sapatos.
     </h2>
   )
 }
 
-export function NavigationMenuDemo() {
+export function TypographyH4() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Menu</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="w-96 scroll-m-20 text-left">
-              <ListItem to="/" title="Home">
-              </ListItem>
-              <ListItem to="/#projects" title="Projetos">
-              </ListItem>
-              <ListItem to="/#about" title="Sobre Mim">
-              </ListItem>
-              <ListItem to="/#freelance" title="Freelance">
-              </ListItem>
-              <ListItem to="/portal" title="Portal Privado">
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem>
-          <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
-            <Link to="/contact">Fale Comigo</Link>
-          </NavigationMenuLink>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+      — Olá
+    </h4>
   )
 }
 
-function ListItem({
-  title,
-  children,
-  to,
-  ...props
-}: React.ComponentPropsWithoutRef<"li"> & { to: string }) {
+export function NavMenu(){
   return (
-    <li {...props}>
-      <NavigationMenuLink asChild>
-        <Link to={to} className="block rounded-md p-3 hover:bg-accent">
-          <div className="text-sm font-medium">{title}</div>
-          <p className="text-sm text-muted-foreground">{children}</p>
-        </Link>
-      </NavigationMenuLink>
-    </li>
+  <>
+    <Button asChild className="fixed top-15 left-10 bg-#5e4ae3">
+      <Link to="/contato">Contato</Link>
+    </Button>
+
+    <Button className="fixed top-15 left-50" variant="ghost">Home</Button>
+  </>
+  )
+}
+
+
+export function NavigationMenuDemo() {
+  return (
+    <NavigationMenu className="font-bold">
+
+          <NavigationMenuItem className="font-bold">
+          <NavigationMenuLink>
+            <Link to="/">Home</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink >
+            <Link to="/projects">Apoie-me</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link to="/projects">Projetos</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link to="/about">Sobre mim</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link to="/freelance">Freelance</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <NavigationMenuLink>
+            <Link to="/portal">Portal privado</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+
+    </NavigationMenu>
   )
 }
 
